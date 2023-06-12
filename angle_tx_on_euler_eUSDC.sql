08f808848d5438cfb4eab542e67c10fcee4eba64
Sébastien LOUIS <louis.sebastien@gmail.com>
2023-06-13 00:19:25 +0200

    Update angle_tx_on_euler_eUSDC.sql

--- angle_tx_on_euler_eUSDC.sql
+++ angle_tx_on_euler_eUSDC.sql
@@ -4,7 +4,11 @@ WITH
   myConstants AS (
     SELECT
       -- Address of the ERC20 compatible eUSDC token (from Euler)
-      0xeb91861f8a4e1c12333f42dce8fb0ecdc28da716 AS tokenAddress
+      0xeb91861f8a4e1c12333f42dce8fb0ecdc28da716 AS tokenAddress,
+      -- Address of the smart contrat of Angle
+      0xf5ad02f3dbbf4b42dee1f1255607f929ca2a7c5a AS angleContract,
+      -- Euler dipatcher contract address
+      0xe5afe81e63f0a52a3a03b922b30f73b8ce74d570 AS eulerDispatcher
   )
 SELECT
   "from",
@@ -20,9 +24,11 @@ WHERE
   AND evt_block_time < CAST('2023-03-14' AS TIMESTAMP)
   -- Check if Angle contract is part of the transaction --
   AND (
-    "from" = 0xf5ad02f3dbbf4b42dee1f1255607f929ca2a7c5a
-    OR "to" = 0xf5ad02f3dbbf4b42dee1f1255607f929ca2a7c5a
+    "from" = myConstants.angleContract
+    OR "to" = myConstants.angleContract
   )
   -- Remove the technical transactions done by Euler dispatcher --
-  AND "from" <> 0xe5afe81e63f0a52a3a03b922b30f73b8ce74d570
-  AND "to" <> 0xe5afe81e63f0a52a3a03b922b30f73b8ce74d570
+  AND (
+    "from" <> myConstants.eulerDispatcher
+    OR "to"<> myConstants.eulerDispatcher
+  )
